85d8015eca49d3e1a2f2eb611e72bec19bc55c35
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-05-14 19:46:19 -0300

    dd

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaResultados = document.querySelector(".texto-resultados");
+
+const perguntas = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas:[
+        "alternativa 1 ",
+        "Alternativa 2 "
+    ]
+},
+{
+    enunciado: "Pergunta 2",
+    alternativas:[
+        "alternativa 1 ",
+        "Alternativa 2 "
+    ]
+}
+    
+]
