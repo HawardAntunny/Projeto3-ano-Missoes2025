755d6cc0d77de1c2c796e234810a61e7da390f2a
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-05-14 19:50:26 -0300

    cc

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-Alternativas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".texto-resultados");
 
 const perguntas = [
     {
-    enunciado: "Pergunta 1",
-    alternativas:[
-        "alternativa 1 ",
-        "Alternativa 2 "
-    ]
-},
-{
-    enunciado: "Pergunta 2",
-    alternativas:[
-        "alternativa 1 ",
-        "Alternativa 2 "
-    ]
-}
-    
+        enunciado: "Por que o campo e a cidade precisam um do outro?",
+        alternativas: [
+            "Porque ambos têm funções distintas, mas complementares.",
+            "Porque o campo sempre depende da cidade para sobreviver."
+        ]
+    },
+    {
+        enunciado: "Qual destas opções representa um produto típico do campo?",
+        alternativas: [
+            "Arroz, feijão e leite.",
+            "Celulares e computadores."
+        ]
+    },
+    {
+        enunciado: "Como a cidade contribui com o campo?",
+        alternativas: [
+            "Com serviços, tecnologia e mercado consumidor.",
+            "Somente com festas e eventos."
+        ]
+    },
+    {
+        enunciado: "O que significa uma conexão saudável entre campo e cidade?",
+        alternativas: [
+            "Trocas justas, respeito e valorização mútua.",
+            "A cidade dominar o campo economicamente."
+        ]
+    },
+    {
+        enunciado: "Como os jovens podem valorizar essa conexão campo-cidade?",
+        alternativas: [
+            "Conhecendo a origem dos alimentos e respeitando quem os produz.",
+            "Ignorando a vida no campo e focando apenas nas cidades grandes."
+        ]
+    }
 ];
