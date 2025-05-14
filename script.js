a721a53f4662056f741068f917aae148d093796c
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-05-14 20:02:11 -0300

    dd

--- script.js
+++ script.js
@@ -7,40 +7,69 @@ const perguntas = [
     {
         enunciado: "Por que o campo e a cidade precisam um do outro?",
         alternativas: [
-            "Porque ambos têm funções distintas, mas complementares.",
-            "Porque o campo sempre depende da cidade para sobreviver."
+            {
+                texto: "Porque ambos têm funções distintas, mas complementares.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Porque o campo sempre depende da cidade para sobreviver.",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Qual destas opções representa um produto típico do campo?",
         alternativas: [
-            "Arroz, feijão e leite.",
-            "Celulares e computadores."
+            {
+                texto: "Arroz, feijão e leite.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Celulares e computadores.",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Como a cidade contribui com o campo?",
         alternativas: [
-            "Com serviços, tecnologia e mercado consumidor.",
-            "Somente com festas e eventos."
+            {
+                texto: "Com serviços, tecnologia e mercado consumidor.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Somente com festas e eventos.",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "O que significa uma conexão saudável entre campo e cidade?",
         alternativas: [
-            "Trocas justas, respeito e valorização mútua.",
-            "A cidade dominar o campo economicamente."
+            {
+                texto: "Trocas justas, respeito e valorização mútua.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "A cidade dominar o campo economicamente.",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Como os jovens podem valorizar essa conexão campo-cidade?",
         alternativas: [
-            "Conhecendo a origem dos alimentos e respeitando quem os produz.",
-            "Ignorando a vida no campo e focando apenas nas cidades grandes."
+            {
+                texto: "Conhecendo a origem dos alimentos e respeitando quem os produz.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Ignorando a vida no campo e focando apenas nas cidades grandes.",
+                afirmacao: "afirmacao"
+            }
         ]
     }
 ];
-
 
 let atual = 0;
 let perguntaAtual;
@@ -48,7 +77,15 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 
+}
+function mostraAlternativas (){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.creatElemnet ("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
 }
 
 mostraPergunta ();
