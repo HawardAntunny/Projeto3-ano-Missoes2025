1a28e64413575537b556c87cffe94d444be37593
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-05-14 20:04:07 -0300

    xx

--- script.js
+++ script.js
@@ -74,18 +74,18 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
+function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
-
 }
-function mostraAlternativas (){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativa = document.creatElemnet ("button");
-        botaoAlternativa.textContent = alternativa.texto;
-        caixaAlternativas.appendChild(botaoAlternativa);
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-mostraPergunta ();
+mostraPergunta();
